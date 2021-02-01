6ed8915db1626e218f60c9fede458e281bd52d7d
honda <hondayuki0606@yahoo.co.jp>
2021-02-01 21:46:00 +0900

    initialize

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.helloworld
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // クリック時の処理
+        findViewById<View>(R.id.morningBtn).setOnClickListener {
+            val messageView = findViewById<TextView>(R.id.greetingText)
+            messageView.text = getString(R.string.good_morning_text);
+
+        }
+        // クリック時の処理
+        findViewById<View>(R.id.afternoonBtn).setOnClickListener {
+            val messageView = findViewById<TextView>(R.id.greetingText)
+            messageView.text = getString(R.string.good_afternoon_text);
+        }
+
+        // クリック時の処理
+        findViewById<View>(R.id.eveningBtn).setOnClickListener {
+            val messageView = findViewById<TextView>(R.id.greetingText)
+            messageView.text = getString(R.string.good_evening_text);
+        }
+    }
+}
---
+++ mylibrary2/src/main/java/com/example/mylibrary2/TestProcess.java
@@ -0,0 +1,8 @@
+package com.example.mylibrary2;
+
+public class TestProcess {
+    public void HelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
+
