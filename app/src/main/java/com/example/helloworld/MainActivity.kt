bf97ffa52b5e1ba6c5cb9015fa57978ea59678e7
honda <hondayuki0606@yahoo.co.jp>
2021-02-13 22:43:34 +0900

    マップ(map) マップ、ミュータブルなマップの実装

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.helloworld
 import android.os.Bundle
-import android.util.Log
+import android.util.Log.d
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.view.menu.ShowableListMenu
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +16,19 @@ class MainActivity : AppCompatActivity() {
             val messageView = findViewById<TextView>(R.id.greetingText)
             messageView.text = getString(R.string.good_morning_text)
 
-            val strs: MutableSet<String> = mutableSetOf("ABC","BCD","CDE")
-            strs.add("CDE")
+            val fruies: Map<String, Int> = mapOf("apple" to 100, "orange" to 80, "banana" to 60)
+            print(fruies["apple"])
             // 実行結果
-            // false
-            print(strs)
+            // 100
+            print(fruies["orange"])
             // 実行結果
-            // [ABC,BCD,CDE]
-            strs.add("DEF")
-            strs.remove("BCD")
+            // 80
+            val mfruies: MutableMap<String, Int> = mutableMapOf("apple" to 100, "orange" to 80, "banana" to 60)
+            mfruies.put("pear", 150)
+            mfruies.remove("orange")
+            d("Print", "mfruies=$mfruies")
             // 実行結果
-            // [ABC,CDE,DEF]
+            // D/Print: mfruies={apple=100, banana=60, pear=150}
 
         }
         // クリック時の処理
