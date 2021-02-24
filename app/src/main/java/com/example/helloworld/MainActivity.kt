c36433002a0c6bf4883511257bbf02954cc9bf55
honda <hondayuki0606@yahoo.co.jp>
2021-02-24 22:12:22 +0900

    ラムダ式

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -29,7 +29,13 @@ class MainActivity : AppCompatActivity() {
             messageView.text = getString(R.string.good_morning_text)
 
             val plus = {x: Int, y: Int -> x + y}
-            Log.d("Tag","plus=" + plus(8,4))
+            Log.d("Tag","plus=" + plus(1,1))
+            val plus2: (Int, Int ) -> Int = {x: Int, y: Int -> x + y}
+            // ラムダ式を省略して記述できる
+            Log.d("Tag","plus=" + plus2(2,2))
+            // 上記のラムダ式を省略して記述できる
+            val plus3: (Int, Int ) -> Int = {x, y -> x + y}
+            Log.d("Tag","plus=" + plus3(3,3))
         }
         // クリック時の処理
         findViewById<View>(R.id.afternoonBtn).setOnClickListener {
